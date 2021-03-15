3378d0570e3fdc9f1fc670054ae2b9e78b9e5b04
zlok00 <65378793+zlok00@users.noreply.github.com>
2021-03-15 16:24:54 +0800

    Add files via upload

---
+++ component/my-foot.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="footer">
+        <div class="wrap">
+            <div class="item clearfix">
+                <div class="box">
+                    <img src="images/logo-white.png"/>
+                </div>
+                <div class="box">
+                    <p>解决方案</p>
+                    <a href="solution.html" target="_blank">智慧农牧</a>
+                    <a href="solution.html" target="_blank">智能消防</a>
+                    <a href="solution.html" target="_blank">智慧养老</a>
+                    <a href="farm-animal.html" target="_blank">农场动物福利</a>
+                </div>
+                <div class="box">
+                    <p>技术优势</p>
+                    <a href="##">低功耗传感器</a>
+                    <a href="##">运动模式识别</a>
+                </div>
+                <div class="box">
+                    <p>青寕支持</p>
+                    <a href="##">维护保障</a>
+                    <a href="##">赋能培训</a>
+                </div>
+                <div class="box">
+                    <p>商务合作</p>
+                    <a href="##">业务咨询</a>
+                    <a href="##">招商加盟</a>
+                    <a href="##">商业案例</a>
+                </div>
+                <div class="box">
+                    <p>关于青寕</p>
+                    <a href="about.html">关于我们</a>
+                    <a href="##">大事记</a>
+                    <a href="##">新闻中心</a>
+                    <a href="##">加入我们</a>
+                </div>
+            </div>
+            <div class="fBox">
+
+                <p class="copy">Copyright© 2020 青寕科技 保留所有权利 &nbsp;| &nbsp;粤ICP备18151626号 &nbsp;| &nbsp;版权所有：深圳青寕信安科技有限公司</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
---
+++ component/my-head.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <div class="wrap clearfix">
+            <a href="index.html" class="logo"><img src="images/logo.jpg" alt="青寕信安" width="170" height="64"></a>
+            <ul class="nav">
+                <li :class="[num==1 ? 'curr' : '']"><a href="##">技术核心</a></li>
+                <li :class="[num==2 ? 'curr' : '']"><a href="##">行业方案</a></li>
+                <li><a href="##">商业案例</a></li>
+                <li :class="[num==4 ? 'curr' : '']"><a href="news.html" target="_blank">新闻中心</a></li>
+                <li :class="[num==5 ? 'curr' : '']"><a href="joinUs.html" target="_blank">加入我们</a></li>
+                <li :class="[num==6 ? 'curr' : '']"><a href="about.html" target="_blank">关于我们</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        props:['keynumber'],
+        data(){
+            return {
+              //  tabnum:1
+                num: this.keynumber
+            }
+        },
+        mounted(){
+            console.log(this.num)
+        }
+    }
+</script>
